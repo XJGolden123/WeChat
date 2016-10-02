44a594faa776f3756e4cfb3a864c99cb18f232eb
杨小军 <yangxiaojun@yangxiaojundemacbook-pro.local>
2016-10-02 13:07:31 +0800

    dog

---
+++ weChat/weChat/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  weChat
+//
+//  Created by 杨小军 on 16/10/2.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+@end
---
+++ weChat/weChat/Dog.m
@@ -0,0 +1,13 @@
+//
+//  Dog.m
+//  weChat
+//
+//  Created by 杨小军 on 16/10/2.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
